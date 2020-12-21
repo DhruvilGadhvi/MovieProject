148ff51c50852840cd67c51af07f54862f149a77
saurabhshinde99 <saurabhshinde208@gmail.com>
2020-12-21 20:31:58 +0530

    set card height

--- static/js/search_data.js
+++ static/js/search_data.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
             console.log(value);
             $("#input").append(`<div class="col mb-3">
                 <div class="card h-100" style="background-color:rgb(26,26,26);">
-                    <img src="https://image.tmdb.org/t/p/w500/${value.poster_path}" class="card-img-top" alt="..." />
+                    <img src="https://image.tmdb.org/t/p/w500/${value.poster_path}" class="card-img-top"style="height:48vh;" alt="..." />
                     <div class="card-body"> 
                         <h5 class="card-title text-center" style="color:white;">'${value.title}'</h5>
                         <p class="card-text" style="color:white;">
