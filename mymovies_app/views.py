6c0dc8e361926b849253c52ad1fbff69fe9fd779
abc <abc@gmail.com>
2020-12-21 14:21:54 +0530

    if date odd->fetch data from api and stores in DB

--- mymovies_app/views.py
+++ mymovies_app/views.py
@@ -4,45 +4,45 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Movie_Data
 from django.views.decorators.csrf import csrf_exempt
-
+import datetime
 
 @csrf_exempt
 def index(request):
     if request.method == 'GET':
         return render(request, 'index.html')
+    if datetime.datetime.now().day % 2 != 0 :
+        # print("odd")
+        # save to data to database
+        response = requests.get(
+            'https://api.themoviedb.org/3/movie/top_rated?api_key=4d8f107a19366ff018ab05901d44eb84&language=en-US&page=1')
+        json_str = json.dumps(response.json(), indent=2)
+        data = json.loads(json_str)
+        for i in range(len(data['results'])):
+            movie_id = data['results'][i]['id']
+            title = data['results'][i]['title']
+            overview = data['results'][i]['overview']
+            popularity = data['results'][i]['popularity']
+            poster_path = data['results'][i]['poster_path']
 
-    # save to data to database
-    '''response = requests.get(
-        'https://api.themoviedb.org/3/movie/top_rated?api_key=4d8f107a19366ff018ab05901d44eb84&language=en-US&page=1')
-    json_str = json.dumps(response.json(), indent=2)
-    data = json.loads(json_str)
-    for i in range(len(data['results'])):
-        movie_id = data['results'][i]['id']
-        title = data['results'][i]['title']
-        overview = data['results'][i]['overview']
-        popularity = data['results'][i]['popularity']
-        poster_path = data['results'][i]['poster_path']
-
-        if not Movie_Data.objects.filter(title=title).exists():
-            Movie_Data.objects.create(
-                title=title, poster_path=poster_path, overview=overview, popularity=popularity)
-            print("Data Created")
+            if not Movie_Data.objects.filter(title=title).exists():
+                Movie_Data.objects.create(
+                    title=title, poster_path=poster_path, overview=overview, popularity=popularity)
+                print("Data Inserted")
 
             else:
                 print("Data Exists...")
-    movie_data = {}
-    for i in range(len(data['results'])):
-        movie_data[data['results'][i]['id']] = {"poster_path": data['results'][i]['poster_path'],
-                                                "title": data['results'][i]['title'],
-                                                "overview": data['results'][i]['overview'],
-                                                "popularity": data['results'][i]['popularity'],
-                                                }
-    movie_data = Movie_Data.objects.all()
-    context = {
-        'movie_data': movie_data,
-    }
-    return HttpResponse("Hello")
-    context = json.dumps(context.json(), indent=2)'''
+    # movie_data = {}
+    # for i in range(len(data['results'])):
+    #     movie_data[data['results'][i]['id']] = {"poster_path": data['results'][i]['poster_path'],
+    #                                             "title": data['results'][i]['title'],
+    #                                             "overview": data['results'][i]['overview'],
+    #                                             "popularity": data['results'][i]['popularity'],
+    #                                             }
+    # movie_data = Movie_Data.objects.all()
+    # context = {
+    #     'movie_data': movie_data,
+    # }
+    # context = json.dumps(context.json(), indent=2)
 
     # Fetch data from database
     movie_data = Movie_Data.objects.all()
